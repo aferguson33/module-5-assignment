4e25d54265e3faa61be216930fd03aefe85eff39
Austin Ferguson <austinferguson500@gmail.com>
2022-08-30 21:47:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,7 +101,10 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      //var chosenCategoryShortName = 
+       var chosenCategoryShortName = function chooseRandomCategory (categories){
+         var randomArrayIndex = Math.floor(Math.random() * categories.length);
+         return categories[randomArrayIndex];
+       }
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
